bd29c84ca4613818260db157e95b69cb501e7bb9
Rémi Blaise <remi.blaise@gmx.fr>
2015-05-16 17:46:28 +0200

    Add a browser warning

---
+++ Resources/public/js/browser_warning.js
@@ -0,0 +1,24 @@
+(function () {
+	var browser_warning = document.getElementById('browser_warning');
+	
+	document.getElementById('whichbrowser').addEventListener("load", function () {
+		var b = new WhichBrowser();
+		
+		if (
+			b.isBrowser('Internet Explorer','<', '11')
+		 || b.isBrowser('Chrome', 			'<', '42')
+		 || b.isBrowser('Firefox', 			'<', '38')
+		 || b.isBrowser('Opera', 			'<', '29')
+		 || b.isBrowser('Safari', 			'<', '8' )
+		) {
+			browser_warning.style.display = 'block';
+		}
+	});
+	
+	buttons = document.getElementsByClassName('goto_site');
+	for ( var i = 0, c = buttons.length; i < c; i++ ) {
+		buttons[i].addEventListener("click", function () {
+			browser_warning.style.display = 'none';
+		});
+	}
+})();
